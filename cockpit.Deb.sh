f053d45057f686a91bf46d3352ba30abaa782730
Rajneesh <40818730+rajneeshmahala@users.noreply.github.com>
2023-09-09 12:53:27 +0000

    update sh

--- cockpit.Deb.sh
+++ cockpit.Deb.sh
@@ -1,1 +1,11 @@
-!#bash  
+#!/bin/bash
+
+echo cockpit setup start
+
+sudo apt update || sudo apt install cockpit -y
+
+sudo systemctl start cockpit
+
+systemctl enable cockpit.socket
+firewall-cmd --permanent --zone=public --add-service=cockpit
+firewall-cmd --reload
